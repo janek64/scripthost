ea9f9039b3fda7b1970a633ac33f4aa92da7c950
Janek Berg <56741217+janek64@users.noreply.github.com>
2022-07-07 15:36:24 +0200

    Create opendjk-version.sh

---
+++ opendjk-version.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Prints the current version for a OpenJDK installation under the provided path
+# Check if the argument was supplied
+if [ $# -lt 1 ];
+then
+	exit 1;
+fi
+# Geth the OpenJDK version
+OPENJDK_VERSION=$($1/java --version | head -n 1 | awk '{print $2}');
+echo $OPENJDK_VERSION;
