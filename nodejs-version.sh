f5b9ad033d962f670f156cb9ab92589ee154a2fe
Janek Berg <56741217+janek64@users.noreply.github.com>
2022-07-07 10:59:09 +0200

    Create nodejs-version.sh

---
+++ nodejs-version.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Prints the current version for a Node.js installation under the provided path
+# Check if the argument is supplied
+if [ $# -lt 1 ];
+then
+	exit 1;
+fi
+# Get the node version
+NODE_VERSION=$($1/node -v)
+# Remove the 'v' starting it
+NODE_VERSION=${NODE_VERSION#?};
+# Print the version
+echo $NODE_VERSION;
